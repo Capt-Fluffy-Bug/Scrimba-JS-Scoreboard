39a0f7656fd6cf2b49d5d34855352592efe8610f
Rahul  Ramakrishnan <rahulramakris@gmail.com>
2023-03-12 11:31:32 -0400

    Extra credit - highlight leader

--- index.js
+++ index.js
@@ -3,37 +3,71 @@ let guestScoreEl = document.getElementById('guest-score')
 let homeScore = 0
 let guestScore = 0
 
+let homeContainer = document.getElementById('home-score-container')
+let guestContainer = document.getElementById('guest-score-container')
+
+homeContainer.classList.toggle('score-container-highlight')
+guestContainer.classList.toggle('score-container-highlight')
+
+function highlightLeader() {
+    homeContainer.classList.toggle('score-container-highlight', homeScore > guestScore)
+    guestContainer.classList.toggle('score-container-highlight', guestScore > homeScore)
+}
+
+
+/* 
+
+    HOME SCORE FUNCTIONS
+
+*/
 function homeAddOne() {
     homeScore += 1;
     homeScoreEl.textContent = homeScore
+    highlightLeader()
 }
 function homeAddTwo() {
     homeScore += 2;
     homeScoreEl.textContent = homeScore
+    highlightLeader()
 }
 function homeAddThree() {
     homeScore += 3;
-    homeScoreEl.textContent = homeScore 
+    homeScoreEl.textContent = homeScore
+    highlightLeader() 
 }
 
 
+/* 
+
+    GUEST SCORE FUNCTIONS
+
+*/
 function guestAddOne() {
     guestScore += 1;
     guestScoreEl.textContent = guestScore
+    highlightLeader()
 }
 function guestAddTwo() {
     guestScore += 2;
     guestScoreEl.textContent = guestScore
+    highlightLeader()
 }
 function guestAddThree() {
     guestScore += 3;
-    guestScoreEl.textContent = guestScore 
+    guestScoreEl.textContent = guestScore
+    highlightLeader() 
 }
 
+/* 
 
+    NEW GAME
+
+*/
 function reset() {
     homeScore = 0
     guestScore = 0
     homeScoreEl.textContent = homeScore
     guestScoreEl.textContent = guestScore
+    homeContainer.classList.remove('score-container-highlight')
+    guestContainer.classList.remove('score-container-highlight')
 }
